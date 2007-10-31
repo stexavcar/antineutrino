fef385b6aa86e59a25e3dc094dec83c338af9a22
plesner@quenta.org <plesner@quenta.org@6d3ad82d-b43d-0410-a727-33fbc2eb2a91>
2007-10-31 06:59:59 +0000

    Fixed line ending style

--- trunk/tools/buildutils.py
+++ trunk/tools/buildutils.py
@@ -1,1 +1,36 @@
-import typesfrom SCons.Script import Split, File# Reads a .list file into an array of stringsdef read_list_file(name):  raw_source = File(name).get_contents()  lines = raw_source.split("\n")  list = []  for line in raw_source.split("\n"):  	trimmed = line.strip()  	if len(trimmed) == 0: continue  	if trimmed[0] == '#': continue  	list.append(trimmed)  return list# Parses a configuration value into a python data structuredef parse_value(str):  str = str.strip()  if str.startswith('[') and str.endswith(']'):    return Split(str.lstrip('[').rstrip(']'))  else:    return str# Apply a set of items read from a configuration file to an# environmentdef apply_items(env, items):  for key, value in items:    key = key.upper()    append = False    if value.startswith('+'):      append = True      value = value.lstrip('+ \t')    value = parse_value(value)    if type(value) is types.StringType: value = ' ' + value    if append: env.Append(**{key: value})    else: env.Replace(**{key: value})
+import types
+from SCons.Script import Split, File
+
+# Reads a .list file into an array of strings
+def read_list_file(name):
+  raw_source = File(name).get_contents()
+  lines = raw_source.split("\n")
+  list = []
+  for line in raw_source.split("\n"):
+  	trimmed = line.strip()
+  	if len(trimmed) == 0: continue
+  	if trimmed[0] == '#': continue
+  	list.append(trimmed)
+  return list
+
+# Parses a configuration value into a python data structure
+def parse_value(str):
+  str = str.strip()
+  if str.startswith('[') and str.endswith(']'):
+    return Split(str.lstrip('[').rstrip(']'))
+  else:
+    return str
+
+# Apply a set of items read from a configuration file to an
+# environment
+def apply_items(env, items):
+  for key, value in items:
+    key = key.upper()
+    append = False
+    if value.startswith('+'):
+      append = True
+      value = value.lstrip('+ \t')
+    value = parse_value(value)
+    if type(value) is types.StringType: value = ' ' + value
+    if append: env.Append(**{key: value})
+    else: env.Replace(**{key: value})
