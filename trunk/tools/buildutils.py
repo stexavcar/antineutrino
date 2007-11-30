0e01f04e1d8185568908d39c20d89cedfe7a04ef
plesner@quenta.org <plesner@quenta.org@6d3ad82d-b43d-0410-a727-33fbc2eb2a91>
2007-11-30 17:50:30 +0000

    Changed format of configuration file

--- trunk/tools/buildutils.py
+++ trunk/tools/buildutils.py
@@ -1,8 +1,8 @@
-import types
+import types, re, sys
 from SCons.Script import Split, File
 
 # Reads a .list file into an array of strings
-def read_list_file(name):
+def read_lines_from(name):
   raw_source = File(name).get_contents()
   lines = raw_source.split("\n")
   list = []
@@ -13,24 +13,77 @@ def read_list_file(name):
   	list.append(trimmed)
   return list
 
-# Parses a configuration value into a python data structure
-def parse_value(str):
-  str = str.strip()
-  if str.startswith('[') and str.endswith(']'):
-    return Split(str.lstrip('[').rstrip(']'))
-  else:
-    return str
+HEADER_PATTERN = re.compile('^(\w+)\s*(\*?)\s*(?:\:\s*([\w\s]+))?{$')
+PROPERTY_PATTERN = re.compile('^(\w+)\s*(\:\=|\+\=)(.*)$')
+
+def read_config_file(name):
+  lines = read_lines_from(name)
+  # Mapping from section names to a mapping from property name to a
+  # pair of the value plus whether or not it is an extension
+  sections = { }
+  # A list of names of targets
+  targets = [ ]
+  index = 0
+  # Function used to signal when an unexpected line occurs
+  def unexpected_line():
+    print "Unexpected line '" + lines[index] + "'"
+    sys.exit(1)
+  # Transforms a string value into a structured value if necessary
+  def parse_value(str):
+    if str.startswith('[') and str.endswith(']'):
+      return str[1:-1].split()
+    else:
+      return str
+  while index < len(lines):
+    header_match = HEADER_PATTERN.match(lines[index])
+    if not header_match: unexpected_line()
+    section = header_match.group(1)
+    is_target = (header_match.group(2) == '*')
+    if is_target: targets.append(section)
+    supers = header_match.group(3)
+    # Read the lines following the section header and load them into
+    # the properties mapping
+    properties = { }
+    # Sets a name to a value using the specified mode
+    def apply_property(name, value, is_extension):
+      if is_extension:
+        if name in properties:
+          (old_value, is_extension) = properties[name]
+          value = old_value + ' ' + value
+        properties[name] = (value, True)
+      else:
+        properties[name] = (value, False)
+    # If there are super sections we run through them and add them
+    # to the mapping for this section
+    if supers:
+      for super in supers.split():
+        for (name, (value, is_extension)) in sections[super].items():
+          apply_property(name, value, is_extension)
+    index = index + 1
+    # Then add the lines read from this section
+    while lines[index] != '}':
+      property_match = PROPERTY_PATTERN.match(lines[index])
+      if not property_match: unexpected_line()
+      name = property_match.group(1)
+      operator = property_match.group(2)
+      value = parse_value(property_match.group(3).strip())
+      apply_property(name, value, operator == '+=')
+      index = index + 1
+    sections[section] = properties
+    index = index + 1
+  # Finally, we reduce the information to a mapping just containing
+  # the targets and with not information about whether or not 
+  # properties were extensions or not
+  result = { }
+  for target in targets:
+    properties = { }
+    for (name, (value, is_extension)) in sections[target].items():
+      properties[name] = value
+    result[target] = properties
+  return result
 
 # Apply a set of items read from a configuration file to an
 # environment
-def apply_items(env, items):
-  for key, value in items:
-    key = key.upper()
-    append = False
-    if value.startswith('+'):
-      append = True
-      value = value.lstrip('+ \t')
-    value = parse_value(value)
-    if type(value) is types.StringType: value = ' ' + value
-    if append: env.Append(**{key: value})
-    else: env.Replace(**{key: value})
+def apply_items(env, properties):
+  for (key, value) in properties.items():
+    env.Replace(**{key.upper(): value})
