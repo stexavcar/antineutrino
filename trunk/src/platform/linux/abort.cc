d328b7c479a2a7fb2241f5f3f67e18e0d038c442
plesner@quenta.org <plesner@quenta.org@6d3ad82d-b43d-0410-a727-33fbc2eb2a91>
2007-12-09 11:41:10 +0000

    A minimal amount of cleanup of linux stack trace code

--- trunk/src/platform/linux/abort.cc
+++ trunk/src/platform/linux/abort.cc
@@ -1,10 +1,6 @@
 #include "platform/posix/abort.cc"
 
 #include <stdlib.h>
-#include <sys/ucontext.h>
-#include <memory.h>
-#include <signal.h>
-#include <execinfo.h>
 #include <dlfcn.h>
 #include <cxxabi.h>
 
@@ -21,18 +17,19 @@ void print_stack_trace(void *ptr, const char *prefix) {
   void *pc = reinterpret_cast<void*>(ucontext->uc_mcontext.gregs[REG_EIP]);
   while (fp && pc) {
     Dl_info info;
-    if (!dladdr(pc, &info)) break;
-    const char *mangled = info.dli_sname;
-    int status = 0;
-    char *demangled = abi::__cxa_demangle(mangled, 0, 0, &status);
-    if (status == 0 && demangled) {
-      remove_substring(demangled, prefix);
-      fprintf(stderr, "%s\n", demangled);
-      free(demangled);
-    } else if (mangled) {
-      fprintf(stderr, "%s(...)\n", mangled);
-      if (strcmp("main", mangled) == 0)
-        break;
+    if (dladdr(pc, &info)) {
+      const char *mangled = info.dli_sname;
+      int status = 0;
+      char *demangled = abi::__cxa_demangle(mangled, 0, 0, &status);
+      if (status == 0 && demangled) {
+        remove_substring(demangled, prefix);
+        fprintf(stderr, "%s\n", demangled);
+        free(demangled);
+      } else if (mangled) {
+        fprintf(stderr, "%s(...)\n", mangled);
+        if (strcmp("main", mangled) == 0)
+          break;
+      }
     }
     pc = fp[1];
     fp = static_cast<void**>(fp[0]);
