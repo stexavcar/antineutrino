a864c2fd56246698aad1c6a69e96cd0c6a9ffe45
plesner@quenta.org <plesner@quenta.org@6d3ad82d-b43d-0410-a727-33fbc2eb2a91>
2009-01-25 14:53:22 +0000

    Finished first service lookup test

--- branches/positron/test/nunit/service-test.cc
+++ branches/positron/test/nunit/service-test.cc
@@ -4,25 +4,41 @@
 
 using namespace neutrino;
 
-/*
-
-class TestObjectProxy : public ObjectProxyDTable<TestObjectProxy> {
+class TestService {
 public:
-  ObjectProxyDTable<TestObjectProxy> &dtable() { return *this; }
+  TestService();
+  ObjectProxyDTable<TestService> &dtable() { return dtable_; }
+  p::Value echo(p::Object self, p::String name, p::Array args);
+private:
+  ObjectProxyDTable<TestService> dtable_;
 };
 
+TestService::TestService() {
+  dtable().add_method("echo", &TestService::echo);
+}
+
+p::Value TestService::echo(p::Object self, p::String name, p::Array args) {
+  assert args.length() == 1;
+  return args[0];
+}
+
 static p::Object make_test_service() {
-  static TestObjectProxy *proxy = NULL;
+  static TestService *proxy = NULL;
   if (proxy == NULL)
-    proxy = new TestObjectProxy();
+    proxy = new TestService();
   return proxy->dtable().to_object(*proxy);
 }
 
-REGISTER_SERVICE(nunit.test, make_test_service);
+REGISTER_SERVICE(nunit.test.echo, make_test_service);
 
 TEST(service_lookup) {
-  p::Object obj = p::ServiceRegistry::lookup("nunit.test");
-  assert (is<p::Null>(obj.send_sync("foo")));
+  p::Object obj = p::ServiceRegistry::lookup("nunit.test.echo");
+  PseudoRandom random(76);
+  for (word i = 0; i < 10; i++) {
+    word n = random.next() % (1 << 24);
+    p::Value response = obj.send("echo", p::Array::of(n));
+    assert (is<p::Integer>(response));
+    assert (cast<p::Integer>(response)).value() == n;
+  }
+  assert (is<p::Void>(obj.send("some_undefined_message")));
 }
-
-*/
